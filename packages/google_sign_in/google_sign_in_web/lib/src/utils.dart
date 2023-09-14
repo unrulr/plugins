5bcc815218e7a70d17aa252bfac4f3d7abd84b58
Will Reppun <will@unrulr.com>
2023-09-14 13:09:43 -1000

    :package: rebase off of google_sign_in 5.4.4

--- packages/google_sign_in/google_sign_in_web/lib/src/utils.dart
+++ packages/google_sign_in/google_sign_in_web/lib/src/utils.dart
@@ -43,7 +43,10 @@ Future<void> injectJSLibraries(
 /// Utility method that converts `currentUser` to the equivalent [GoogleSignInUserData].
 ///
 /// This method returns `null` when the [currentUser] is not signed in.
-GoogleSignInUserData? gapiUserToPluginUserData(auth2.GoogleUser? currentUser) {
+GoogleSignInUserData? gapiUserToPluginUserData(
+  auth2.GoogleUser? currentUser, {
+  String? serverAuthCode,
+}) {
   final bool isSignedIn = currentUser?.isSignedIn() ?? false;
   final auth2.BasicProfile? profile = currentUser?.getBasicProfile();
   if (!isSignedIn || profile?.getId() == null) {
@@ -56,5 +59,6 @@ GoogleSignInUserData? gapiUserToPluginUserData(auth2.GoogleUser? currentUser) {
     id: profile?.getId() ?? '',
     photoUrl: profile?.getImageUrl(),
     idToken: currentUser?.getAuthResponse().id_token,
+    serverAuthCode: serverAuthCode,
   );
 }
